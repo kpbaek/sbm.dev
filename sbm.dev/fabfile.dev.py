49c7fd92e72731fd1fe225e3acf11ec3ed79334c
KPBAEK <KPBAEK@sbmkorea.com>
2014-06-13 15:45:59 +0900

    .

---
+++ sbm.dev/fabfile.dev.py
@@ -0,0 +1,14 @@
+from fabric.api import local, lcd
+
+def prepare_deployment(branch_name):
+    local('git add -A && git commit')
+    local('git checkout master && git merge ' + branch_name)
+    
+def deploy():
+    with lcd('/home/sbmkorea/public_html/plts.sbmkorea.com/'):
+        local('rm -rf sbm')
+#        local('git clone /home/sbmkorea/DJANGO_ENV/projects/sbm/')
+    with lcd('/home/sbmkorea/public_html/plts.sbmkorea.com/sbm'):
+        local('python manage.py migrate plts')
+        local('python manage.py collectstatic')
+        local('sudo service apache2 reload')
---
+++ sbm.dev/fabfile.local.py
@@ -0,0 +1,15 @@
+from fabric.api import local, lcd
+
+def prepare_deployment(branch_name):
+    local('git add -A && git commit')
+    local('git checkout master && git merge ' + branch_name)
+    
+def deploy():
+#    with lcd('/home/sbmkorea/public_html/plts.sbmkorea.com/'):
+#        local('rm -rf sbm')
+#        local('git clone /home/sbmkorea/DJANGO_ENV/projects/sbm/')
+#    with lcd('/home/sbmkorea/public_html/plts.sbmkorea.com/sbm'):
+    with lcd('D:/dev/python/web/sbm/sbm.dev'):
+        local('python manage.py migrate plts')
+        local('python manage.py collectstatic')
+#        local('sudo service apache2 reload')
